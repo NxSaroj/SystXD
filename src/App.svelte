50f4a71f0a9f30c4f446445508ad6b0bf721f183
Syst69 <148795898+Syst69@users.noreply.github.com>
2024-03-13 20:17:55 +0530

    Files Uploaded

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Button from "./lib/Button.svelte";
+  import Title from "./lib/Title.svelte";
+  import './app.css'
+</script>
+
+<Title />
+<Button />
---
+++ src/lib/Title.svelte
@@ -0,0 +1,1 @@
+<h1 class="transition-all ease-in-out text-5xl text-white font-bold mt-64 text-center">Hey I'm Syst</h1>
