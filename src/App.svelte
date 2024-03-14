1137b2c5e44eb322e4e332061b86b2905b314c0b
Syst69 <148795898+Syst69@users.noreply.github.com>
2024-03-14 11:55:01 +0530

    Added skills section

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script>
   import Button from "./lib/Button.svelte";
   import Title from "./lib/Title.svelte";
-  import './app.css'
+  import Border from "./lib/Border.svelte";
+  import "./app.css";
 </script>
 
 <Title />
 <Button />
+<h1
+  class="transition-all ease-in-out text-3xl text-white font-bold mt-12 text-center underline"
+>
+  Tools
+</h1>
+
+<Border />
---
+++ src/lib/Border.svelte
@@ -0,0 +1,11 @@
+<div class="w-full gap-10 border h-24 mt-10 flex items-center justify-center">
+  <i
+    class="fa-brands transition-all ease-linear fa-git-alt text-5xl hover:text-6xl"
+  ></i>
+  <i
+    class="fa-brands fa-node-js transition-all ease-linear hover:text-6xl text-5xl"
+  ></i>
+  <i
+    class="transition-all ease-linear fa-brands fa-python text-5xl hover:text-6xl"
+  ></i>
+</div>
